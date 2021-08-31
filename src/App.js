8e8a7ec1b16107e0ac89420e09e01527f7c32c17
Fahmiali99 <79827631+Fahmiali99@users.noreply.github.com>
2021-08-31 19:16:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,170 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const titleSepatu = "Jordan Dior";
+  const harga = "$9123";
+  const judul = "ALL SHOES";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div class="card text-center">
+      <div class="card-header">
+        <ul class=" nav nav-tabs card-header-tabs kanan">
+          <li class="nav-item">
+            <a class="nav-link nav-link active" href="#">
+              All
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">
+              Dior
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">
+              Nike
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title judul-page">{judul}</h5>
+        <p class="container card-text mt-2">
+        You can judge whether a woman is elegant by looking at her feet", wrote Christian Dior in The Little Dictionary of Fashion. Dior shoes are the essential finishing touch for a feminine look
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div class="card-group mt-5">
+          <div class="card">
+            <img src="assets/img/ddior.png"></img>
+            <div class="card-body">
+              <h5 class="card-title">{titleSepatu}</h5>
+              <p class="price">USD {harga}</p>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <button class="btn btn-outline-primary mr-1 active keranjang" href="#">
+                Buy
+              </button>
+              <button class="btn btn-outline-primary" href="#">
+              <i class="fas fa-cart-plus"></i>
+              </button>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="assets/img/ddior.png"></img>
+            <div class="card-body">
+              <h5 class="card-title">{titleSepatu}</h5>
+              <p class="price">USD {harga}</p>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <button class="btn btn-outline-primary mr-1 active keranjang" href="#">
+                Buy
+              </button>
+              <button class="btn btn-outline-primary" href="#">
+              <i class="fas fa-cart-plus"></i>
+              </button>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="assets/img/ddior.png"></img>
+            <div class="card-body">
+              <h5 class="card-title">{titleSepatu}</h5>
+              <p class="price">USD {harga}</p>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <button class="btn btn-outline-primary mr-1 active keranjang" href="#">
+                Buy
+              </button>
+              <button class="btn btn-outline-primary" href="#">
+              <i class="fas fa-cart-plus"></i>
+              </button>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-group">
+          <div class="card ">
+            <img src="assets/img/ddior.png"></img>
+            <div class="card-body">
+              <h5 class="card-title">{titleSepatu}</h5>
+              <p class="price">USD {harga}</p>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <button class="btn btn-outline-primary mr-1 active keranjang" href="#">
+                Buy
+              </button>
+              <button class="btn btn-outline-primary" href="#">
+              <i class="fas fa-cart-plus"></i>
+              </button>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="assets/img/ddior.png"></img>
+            <div class="card-body">
+              <h5 class="card-title">{titleSepatu}</h5>
+              <p class="price">USD {harga}</p>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <button class="btn btn-outline-primary mr-1 active keranjang" href="#">
+                Buy
+              </button>
+              <button class="btn btn-outline-primary" href="#">
+              <i class="fas fa-cart-plus"></i>
+              </button>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="assets/img/ddior.png"></img>
+            <div class="card-body">
+              <h5 class="card-title">{titleSepatu}</h5>
+              <p class="price">USD {harga}</p>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <button class="btn btn-outline-primary mr-1 active keranjang" href="#">
+                Buy
+              </button>
+              <button class="btn btn-outline-primary" href="#">
+              <i class="fas fa-cart-plus"></i>
+              </button>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
